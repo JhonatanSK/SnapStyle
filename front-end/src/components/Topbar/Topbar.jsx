8091613ab6aab24548caf27dee0ca44795295ad5
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-05-29 00:53:45 -0300

    feature: corrigindo o header e adicionando a logo da marca

--- front-end/src/components/Topbar/Topbar.jsx
+++ front-end/src/components/Topbar/Topbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import './Topbar.css';
+import './Topbar.scss';
 import { ReactComponent as Search } from '../../assets/search-white-24dp.svg';
 import { ReactComponent as Bag } from '../../assets/bag.svg';
+import { ReactComponent as LogoDark } from '../../assets/logo-dark.svg';
 
 function Topbar() {
   return (
     <header className="topbar">
         <nav className="topbar__menu">
-            <p>SNAPSTYLE</p>
+          <a><LogoDark alt="Logo" /></a>
             <div className="topbar__icons">
                 <Search alt="search" />
                 <Bag alt="bag" />
